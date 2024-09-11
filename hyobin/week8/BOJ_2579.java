bf88adf813655744a3590a064c889634cad0af1a
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-09-11 21:25:55 +0900

    BOJ 2579 S3 계단오르기

---
+++ hyobin/week8/BOJ_2579.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class 계단오르기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 계단의 개수
+        int[] dp = new int[n+1];
+        int[] arr = new int[n+1];
+        for(int i=1; i<=n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        dp[1] = arr[1];
+        if(n >= 2){
+        dp[2] = arr[1] + arr[2];}
+        if(n >= 3){
+        for(int i=3; i<=n; i++) {
+          //전전+현재, 전전전+전+현재 비교 => 연속된 전전+전+현재는 안되기 때문
+            dp[i] = Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]);
+        }
+        }
+        System.out.println(dp[n]);
+
+    }
+}
