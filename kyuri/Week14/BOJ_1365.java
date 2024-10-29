0323b7d188b411e748b59cdfd4fe695edbec0eda
kyuri-Kwon <68770864+rnjs010@users.noreply.github.com>
2024-10-29 23:26:35 +0900

    BOJ 1365 G2 꼬인 전깃줄

---
+++ kyuri/Week14/BOJ_1365.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(st.nextToken()) - 1] = i;
+        }
+
+        int[] LIS = new int[N];
+        LIS[0] = arr[0];
+        int ans = 1;
+        for (int i = 1; i < N; i++) {
+            int key = arr[i];
+            if (LIS[ans - 1] < key) {
+                LIS[ans] = key;
+                ans++;
+            } else {
+                // Lovwer bound
+                int l = 0, r = ans - 1;
+                while (l <= r) {
+                    int mid = (l + r) / 2;
+                    if (LIS[mid] < key) {
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                LIS[l] = key;
+            }
+        }
+
+        System.out.println(N - ans);
+    }
+
+}
