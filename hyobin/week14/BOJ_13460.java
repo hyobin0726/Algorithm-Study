2ba892f9118fa508006f018fa5b678626407730b
hyobin <140376727+hyobin0726@users.noreply.github.com>
2024-10-29 14:18:33 +0900

    BOJ 13460 G1 구슬탈출2

--- hyobin/week14/BOJ_13460.java
+++ hyobin/week14/BOJ_13460.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int n,m;
+    static int n,m =0;
     static char[][] map;
     static int[] dx= {-1,1,0,0};
     static int[] dy= {0,0,-1,1};
